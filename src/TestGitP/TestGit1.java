82d88afab5dac133aa75eb56cc948f45ecf39694
cuneiform-ronakpatel <rp6038272@gmail.com>
2024-09-12 17:40:59 +0530

    hi

---
+++ src/TestGitP/TestGit1.java
@@ -0,0 +1,28 @@
+package TestGitP;
+
+public class TestGit1 
+
+{
+	
+	public void chrome()
+	{
+		int a=10;
+		int b=20;
+		int c=0;
+		
+		c=a+b;
+		
+		System.out.println(c);	
+		
+		
+	}
+
+	public static void main(String[] args) 
+	
+	{
+		// TODO Auto-generated method stub
+		TestGit1 obj=new TestGit1();
+		obj.chrome();
+	}
+
+}
